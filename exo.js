b315c49093074716e81ed12b1126767590398b9a
yanntyb <69966391+yanntyb@users.noreply.github.com>
2020-12-14 10:30:13 +0100

    a

---
+++ exo.js
@@ -0,0 +1,14 @@
+console.log(1,Math.trunc(34.589456));
+console.log(2,Math.trunc(68.412784));
+console.log(3,Math.ceil(39.456));
+console.log(4, Math.ceil(27.34876541));
+console.log(5, Math.round(5.68));
+console.log(6, Math.round(5.41));
+console.log(7, parseInt(6.12457));
+console.log(8,parseInt(9.568124));
+console.log(9,Math.min(5, 1, 12, -1, 45));
+console.log(9,Math.min(9, 12, 16, 44, 3));
+console.log(10,Math.max(5, 1, 12, -1, 45));
+console.log(11,Math.max(9, 12, 16, 44, 3));
+console.log(12,Math.abs(-6));
+console.log(13,Math.abs(-56));
